81045a9678b8b03fad2d9adab2cf50754a135181
YA <9039417+yzkn@users.noreply.github.com>
2023-12-24 18:42:28 +0900

    Updates

--- js/app.js
+++ js/app.js
@@ -4,6 +4,136 @@
 const keyPrefix = 'NOTEPAD_';
 
 
+const DEFAULT_FONT_FAMILIES = new Set([
+    // Windows
+    'Arial',
+    'Arial Black',
+    'Bahnschrift',
+    'Calibri',
+    'Cambria',
+    'Cambria Math',
+    'Candara',
+    'Comic Sans MS',
+    'Consolas',
+    'Constantia',
+    'Corbel',
+    'Courier New',
+    'Ebrima',
+    'Franklin Gothic Medium',
+    'Gabriola',
+    'Gadugi',
+    'Georgia',
+    'HoloLens MDL2 Assets',
+    'Impact',
+    'Ink Free',
+    'Javanese Text',
+    'Leelawadee UI',
+    'Lucida Console',
+    'Lucida Sans Unicode',
+    'Malgun Gothic',
+    'Marlett',
+    'Microsoft Himalaya',
+    'Microsoft JhengHei',
+    'Microsoft New Tai Lue',
+    'Microsoft PhagsPa',
+    'Microsoft Sans Serif',
+    'Microsoft Tai Le',
+    'Microsoft YaHei',
+    'Microsoft Yi Baiti',
+    'MingLiU-ExtB',
+    'Mongolian Baiti',
+    'MS Gothic',
+    'MV Boli',
+    'Myanmar Text',
+    'Nirmala UI',
+    'Palatino Linotype',
+    'Segoe MDL2 Assets',
+    'Segoe Print',
+    'Segoe Script',
+    'Segoe UI',
+    'Segoe UI Historic',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'SimSun',
+    'Sitka',
+    'Sylfaen',
+    'Symbol',
+    'Tahoma',
+    'Times New Roman',
+    'Trebuchet MS',
+    'Verdana',
+    'Webdings',
+    'Wingdings',
+    'Yu Gothic',
+
+    // Mac
+    'American Typewriter',
+    'Andale Mono',
+    'Arial',
+    'Arial Black',
+    'Arial Narrow',
+    'Arial Rounded MT Bold',
+    'Arial Unicode MS',
+    'Avenir',
+    'Avenir Next',
+    'Avenir Next Condensed',
+    'Baskerville',
+    'Big Caslon',
+    'Bodoni 72',
+    'Bodoni 72 Oldstyle',
+    'Bodoni 72 Smallcaps',
+    'Bradley Hand',
+    'Brush Script MT',
+    'Chalkboard',
+    'Chalkboard SE',
+    'Chalkduster',
+    'Charter',
+    'Cochin',
+    'Comic Sans MS',
+    'Copperplate',
+    'Courier',
+    'Courier New',
+    'Didot',
+    'DIN Alternate',
+    'DIN Condensed',
+    'Futura',
+    'Geneva',
+    'Georgia',
+    'Gill Sans',
+    'Helvetica',
+    'Helvetica Neue',
+    'Herculanum',
+    'Hoefler Text',
+    'Impact',
+    'Lucida Grande',
+    'Luminari',
+    'Marker Felt',
+    'Menlo',
+    'Microsoft Sans Serif',
+    'Monaco',
+    'Noteworthy',
+    'Optima',
+    'Palatino',
+    'Papyrus',
+    'Phosphate',
+    'Rockwell',
+    'Savoye LET',
+    'SignPainter',
+    'Skia',
+    'Snell Roundhand',
+    'Tahoma',
+    'Times',
+    'Times New Roman',
+    'Trattatello',
+    'Trebuchet MS',
+    'Verdana',
+    'Zapfino',
+
+    // Google Fonts
+    'Source Code Pro'
+].sort());
+
+
 const lsTest = _ => {
     localStorage.setItem(keyPrefix + 'copyright', 'Copyright (c) 2023 YA All rights reserved.');
     localStorage.setItem(keyPrefix + 'markdown', '# Title\n\n----------\n\nContent');
@@ -73,6 +203,20 @@ const downloadFile = (key, content) => {
 };
 
 
+const listFontFamilies = async () => {
+    const datalist = document.getElementById('fontFamilies');
+
+    await document.fonts.ready;
+    for (const font of DEFAULT_FONT_FAMILIES.values()) {
+        if (document.fonts.check(`12px "${font}"`)) {
+            const option = document.createElement('option');
+            option.value = font;
+            datalist.appendChild(option);
+        }
+    }
+};
+
+
 window.addEventListener('DOMContentLoaded', _ => {
 
     // test
@@ -96,11 +240,18 @@ window.addEventListener('DOMContentLoaded', _ => {
     });
 
 
+    document.getElementById('fontFamily').addEventListener('change', () => {
+        document.getElementById('mainContent').style.fontFamily = document.getElementById('fontFamily').value;
+    });
+
+
     // datalistの再読込
     document.getElementById('reload').addEventListener('click', (event) => {
         listLocalStorageKeys();
     });
     listLocalStorageKeys();
+
+    listFontFamilies();
 
 
     // ファイル
